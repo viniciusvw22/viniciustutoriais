874aead25e2160fd74ba6e2569fa3f399c2b1b02
Vinícius Werneck <viniciusvw18@hotmail.com>
2021-01-08 11:16:51 -0300

    Add link to GitHub page
    Add link to GitHub page and update navigation menu size.

--- _templates/menu-nav.php
+++ _templates/menu-nav.php
@@ -6,6 +6,7 @@
       <nav id="menu">
         <ul>
           <li><a href="../home.php">Home</a></li>
+          <li><a href="https://github.com/viniciusvw22/viniciustutoriais/blob/main/home.php">Código no GitHub!</a></li>
           <li><a href="tutoriais-html">HTML-CSS-Javascript</a></li>
           <li><a href="tutoriais-historia">História</a></li>
           <li><a href="contato.html">Contato</a></li>
